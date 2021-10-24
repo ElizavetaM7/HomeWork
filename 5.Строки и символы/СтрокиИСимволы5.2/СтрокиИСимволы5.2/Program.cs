01c65bcdfa004c2a6e2bde50527696a9f9c1ec5c
Elizaveta.Mamzeleva@urfu.me <Elizaveta.Mamzeleva@urfu.me>
2021-10-24 10:44:25 +0500

    Задание 3

---
+++ 5.Строки и символы/СтрокиИСимволы5.2/СтрокиИСимволы5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace СтрокиИСимволы5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
